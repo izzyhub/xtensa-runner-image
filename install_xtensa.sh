42a3342493cbd1272b43143ecd86e3980305fd02
Izzy <izzy@izzywatson.com>
2024-09-09 21:04:48 -0400

    Move things around and run espup with the llvm flag.
    Locally at least this seems to fix the issues with cargo-tarpaulin
    building. So hopefully now I can get code coverage.

--- install_xtensa.sh
+++ install_xtensa.sh
@@ -7,9 +7,9 @@ rustup set profile complete
 curl -L https://github.com/esp-rs/espup/releases/latest/download/espup-x86_64-unknown-linux-gnu -o "$HOME/.cargo/bin/espup"
 chmod a+x "$HOME/.cargo/bin/espup"
 source "$HOME/.cargo/env"
-"$HOME/.cargo/bin/espup" install -l debug --export-file $HOME/exports
+echo 'export RUSTFLAGS="-C instrument-coverage=all"' >>$HOME/.bashrc
+"$HOME/.cargo/bin/espup" install --extended-llvm --log-level debug --export-file $HOME/exports
 touch $HOME/.bashrc
-echo 'export RUSTFLAGS="-C instrument-coverage=all"'
 echo "source $HOME/exports" >>$HOME/.bashrc
 echo "$PATH" >>"$GITHUB_PATH"
 #echo "$PATH" >> "$GITEA_PATH"
